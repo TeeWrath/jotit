08bcfb21c0626cb82eb146249e32a181ca2ceb0f
Subroto Banerjee <subroto.2003@gmail.com>
2025-10-01 20:01:00 +0530

    feat: setup middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,19 @@ import express from "express"
 import noteRoutes from "./routes/noteRoutes.js"
 import { connectDB } from "./config/db.js"
 import dotenv from "dotenv"
+import ratelimiter from "./middleware/ratelimiter.js"
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5001
-connectDB();
 
 app.use(express.json());
+app.use(ratelimiter)
 app.use("/api/notes", noteRoutes)
 
-app.listen(PORT, () => {
-    console.log("server is running on port: ",PORT)
-})
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("server is running on port: ", PORT)
+    })
+});
