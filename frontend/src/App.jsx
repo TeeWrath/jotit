5b81a6f22af5c929d68dfd73dc61b0c047ec4f94
Subroto Banerjee <subroto.2003@gmail.com>
2025-10-02 20:09:22 +0530

    tailwind is finally working huh

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import { Route, Routes } from 'react-router'
 import HomePage from './pages/HomePage'
 import CreatePage from './pages/CreatePage'
-import toast from 'react-hot-toast'
+// import toast from 'react-hot-toast'
 
 const App = () => {
   return (
     <div>
-      <button onClick={()=>toast.success("Done bitch")}>Click</button>
+      {/* Test regular CSS */}
+      <div className="test-class">This should be red and bold</div>
+      
+      {/* Test Tailwind */}
+      <button className='bg-blue-500 text-white p-4 rounded-lg font-bold'>
+        Tailwind Test Button
+      </button>
+      
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/create' element={<CreatePage/>}/>
