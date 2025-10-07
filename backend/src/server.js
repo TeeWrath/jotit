ab61da029ac9cd3587e51a743be9fba200de48ac
Subroto Banerjee <subroto.2003@gmail.com>
2025-10-07 20:37:14 +0530

    feat: homepage and related configs

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+
 import noteRoutes from "./routes/noteRoutes.js"
 import { connectDB } from "./config/db.js"
-import dotenv from "dotenv"
 import ratelimiter from "./middleware/ratelimiter.js"
 
 dotenv.config()
@@ -9,6 +11,9 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5001
 
+app.use(cors({
+    origin:"http://localhost:5173",
+} ))
 app.use(express.json());
 app.use(ratelimiter)
 app.use("/api/notes", noteRoutes)
